f7cdff5e9bed2fa8d94c09505804416a26e3a367
tom <tom.tam@2fi-solutions.com.hk>
2021-12-29 18:35:36 +0800

    Add basic variable

--- main.cpp
+++ main.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "Mike" << endl;
+
+//    variable
+    string characterName = "John";
+    int characterAge = 35;
+    cout << "Hello, " << characterName << " I'm " << characterAge << endl;
+    cout << "Hello, " << characterAge << endl;
+
+
+
     return 0;
 }
